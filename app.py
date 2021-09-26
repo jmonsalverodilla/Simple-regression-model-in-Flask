52836963f3bfc56f5cd733bf6817f384265570d6
Javier <javiermonsalverodilla@gmail.com>
2021-09-26 17:43:24 +0200

    update

--- app.py
+++ app.py
@@ -25,5 +25,5 @@ def predict_expenses():
 
 if __name__ == "__main__":
     port = os.environ.get("PORT", 8080)
-    app.run(debug=True, host="0.0.0.0", port=port)
+    app.run(debug=False, host="0.0.0.0", port=port)
 
